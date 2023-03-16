506cf7a6943eabb33a3bc94f2ac388f78eda046f
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-03-16 19:29:53 +0200

    Add files via upload

---
+++ Project 2/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Project_2
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.close_btn = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.PictureBox();
+            this.stop = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.status = new System.Windows.Forms.Label();
+            this.toTypeLabel = new System.Windows.Forms.PictureBox();
+            this.toType = new System.Windows.Forms.Label();
+            this.whatYouTypeLabel = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.whatYouType = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.total = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.close_btn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toTypeLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.whatYouTypeLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // close_btn
+            // 
+            this.close_btn.BackColor = System.Drawing.Color.Transparent;
+            this.close_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close_btn.Image = ((System.Drawing.Image)(resources.GetObject("close_btn.Image")));
+            this.close_btn.Location = new System.Drawing.Point(756, 12);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(32, 29);
+            this.close_btn.TabIndex = 0;
+            this.close_btn.TabStop = false;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(689, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 15);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.minimize_click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(347, 83);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(43, 85);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(706, 29);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Project_2.Properties.Resources.Rectangle_9__1_;
+            this.pictureBox4.Location = new System.Drawing.Point(-1, 116);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(143, 389);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Transparent;
+            this.start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
+            this.start.Location = new System.Drawing.Point(208, 87);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(200, 84);
+            this.start.TabIndex = 5;
+            this.start.TabStop = false;
+            this.start.Click += new System.EventHandler(this.start_click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Transparent;
+            this.stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.stop.Image = ((System.Drawing.Image)(resources.GetObject("stop.Image")));
+            this.stop.Location = new System.Drawing.Point(447, 87);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(200, 84);
+            this.stop.TabIndex = 6;
+            this.stop.TabStop = false;
+            this.stop.Click += new System.EventHandler(this.stop_click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(160, 161);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(533, 10);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(285, 491);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(144, 34);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.Font = new System.Drawing.Font("Lucida Sans Typewriter", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(409, 486);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(153, 23);
+            this.status.TabIndex = 9;
+            this.status.Text = "Not Playing";
+            // 
+            // toTypeLabel
+            // 
+            this.toTypeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.toTypeLabel.Image = ((System.Drawing.Image)(resources.GetObject("toTypeLabel.Image")));
+            this.toTypeLabel.Location = new System.Drawing.Point(300, 177);
+            this.toTypeLabel.Name = "toTypeLabel";
+            this.toTypeLabel.Size = new System.Drawing.Size(347, 83);
+            this.toTypeLabel.TabIndex = 10;
+            this.toTypeLabel.TabStop = false;
+            this.toTypeLabel.Visible = false;
+            // 
+            // toType
+            // 
+            this.toType.AutoSize = true;
+            this.toType.BackColor = System.Drawing.Color.Transparent;
+            this.toType.Font = new System.Drawing.Font("Lucida Sans Typewriter", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toType.ForeColor = System.Drawing.Color.Gray;
+            this.toType.Location = new System.Drawing.Point(150, 214);
+            this.toType.MaximumSize = new System.Drawing.Size(550, 82);
+            this.toType.Name = "toType";
+            this.toType.Size = new System.Drawing.Size(0, 23);
+            this.toType.TabIndex = 11;
+            // 
+            // whatYouTypeLabel
+            // 
+            this.whatYouTypeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.whatYouTypeLabel.Image = ((System.Drawing.Image)(resources.GetObject("whatYouTypeLabel.Image")));
+            this.whatYouTypeLabel.Location = new System.Drawing.Point(300, 325);
+            this.whatYouTypeLabel.Name = "whatYouTypeLabel";
+            this.whatYouTypeLabel.Size = new System.Drawing.Size(347, 83);
+            this.whatYouTypeLabel.TabIndex = 12;
+            this.whatYouTypeLabel.TabStop = false;
+            this.whatYouTypeLabel.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::Project_2.Properties.Resources.bar;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 128);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(110, 361);
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            // 
+            // whatYouType
+            // 
+            this.whatYouType.AutoSize = true;
+            this.whatYouType.BackColor = System.Drawing.Color.Transparent;
+            this.whatYouType.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.whatYouType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.whatYouType.Location = new System.Drawing.Point(156, 387);
+            this.whatYouType.MaximumSize = new System.Drawing.Size(550, 106);
+            this.whatYouType.Name = "whatYouType";
+            this.whatYouType.Size = new System.Drawing.Size(0, 21);
+            this.whatYouType.TabIndex = 14;
+            this.whatYouType.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 161);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(95, 221);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.accuracy_KeyPress);
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.BackColor = System.Drawing.Color.Transparent;
+            this.total.Font = new System.Drawing.Font("Lucida Sans Typewriter", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.ForeColor = System.Drawing.Color.Black;
+            this.total.Location = new System.Drawing.Point(30, 461);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(36, 23);
+            this.total.TabIndex = 16;
+            this.total.Text = "0%";
+            // 
+            // mainWindow
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 517);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.whatYouType);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.whatYouTypeLabel);
+            this.Controls.Add(this.toType);
+            this.Controls.Add(this.toTypeLabel);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.close_btn);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "mainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mainWindow_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.close_btn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toTypeLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.whatYouTypeLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox close_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox start;
+        private System.Windows.Forms.PictureBox stop;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.PictureBox toTypeLabel;
+        private System.Windows.Forms.Label toType;
+        private System.Windows.Forms.PictureBox whatYouTypeLabel;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label whatYouType;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label total;
+    }
+}
+
---
+++ Project 2/Form1.cs
@@ -0,0 +1,217 @@
+﻿/*
+    Author: Pieter Malan
+    Filename: Form1.cs
+    Description: This file includes the main window class and all the methods, events connected to it.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Project_2
+{
+    public partial class mainWindow : Form
+    {
+        public float wrong = 0; // wrong is the amount incorrect, but it doesn't reset to 0 unless game ended. Used for average.
+        public float counter = 0; // counter is used to end the game when all letters are typed
+        public float average = 0; // average gets updated and displayed on screen
+        public float wrongCounter = 0; // wrongCounter is the amount incorrect, but it is used to refresh typed letters until the correct one is typed.
+        public Hashtable letters = new Hashtable(); // hashtable to store all the letters
+        bool started = false; // bolean used to start/stop game
+        string[] sentences = { "She wondered what his eyes were saying beneath his mirrored sunglasses.", "The light that burns twice as bright burns half as long." , "He felt that dining on the bridge brought romance to his relationship with his cat.", "It doesn't sound like that will ever be on my travel list.", "Cats are good pets, for they are clean and are not noisy.", "Bill ran from the giraffe toward the dolphin.", "The elderly neighborhood became enraged over the coyotes who had been blamed for the poodle's disappearance.", "Her fragrance of choice was fresh garlic.", "The toy brought back fond memories of being lost in the rain forest.", "Red is greener than purple, for sure." };
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // Close button top-right
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Minimize button top-right
+        private void minimize_click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // Start game button
+        private void start_click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+
+                for (int v = 0; v < listBox1.Items.Count; v++)
+                {
+                    listBox1.Items.RemoveAt(v);
+                    v--;
+                }
+
+                GC.Collect(); // clear previously saved hashtables?
+                letters = new Hashtable();
+
+                toTypeLabel.Visible = true;
+                whatYouTypeLabel.Visible = true;
+                whatYouType.Visible = true;
+                toType.Visible = true;
+                started = true;
+                wrong = 0;
+                wrongCounter = 0;
+                status.Text = "Playing";
+                Random random = new Random();
+                toType.Text = this.sentences[random.Next(0,10)];
+
+                average = 100-(wrong/toType.Text.Length*100);
+                average = (float)Math.Round(average, 2);
+                total.Text = (average.ToString() + "%");
+
+                for (int x = 0; x < toType.Text.Length; x++)
+                {
+                    Console.WriteLine(toType.Text[x]);
+                    try {
+                        letters.Add(toType.Text[x], toType.Text[x] + " - 100");
+                    }
+                    catch
+                    {
+                        //ignore duplicate values for letters in listbox error
+                    }
+                }
+
+                foreach (var item in letters.Values)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+
+        }
+
+        // Stop game button
+        private void stop_click(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                counter = 0;
+                total.Text = "";
+                whatYouType.Visible = false;
+                toTypeLabel.Visible = false;
+                whatYouTypeLabel.Visible = false;
+                started = false;
+                status.Text = "Not Playing";
+                toType.Text = "";
+                whatYouType.Text = "";
+            }
+
+        }
+
+        // track keyboard input
+        private void Check_Key(KeyPressEventArgs e)
+        {
+            if (started)
+            {
+                if (counter >= (toType.Text.Length)-1)
+                {
+                    counter = 0;
+                    started = false;
+                    whatYouType.Text = "";
+                    toType.Text = "";
+                    whatYouType.Visible = false;
+                    toType.Visible = false;
+                    toTypeLabel.Visible = false;
+                    whatYouTypeLabel.Visible = false;
+                    status.Text = "Not Playing";
+                }
+                else
+                {
+                    if (e.KeyChar == toType.Text[((int)counter)])
+                    {
+                        if (wrongCounter >= 1)
+                        {
+                            whatYouType.Text = whatYouType.Text.Remove(whatYouType.Text.Length - 1,1);
+                            whatYouType.ForeColor = Color.FromArgb(1, 64, 64, 64);
+                        }
+                        wrongCounter = 0;
+                        whatYouType.Text += e.KeyChar;
+                        counter += 1;
+                        average = 100-(wrong/toType.Text.Length*100);
+                        average = (float)Math.Round(average, 2);
+                        total.Text = (average.ToString() + "%");
+
+                    }
+                    else
+                    {
+                        wrongCounter++;
+                        wrong++;
+                        average = 100-(wrong/toType.Text.Length*100);
+                        whatYouType.ForeColor = Color.Red;
+                        
+                        if (average < 0)
+                        {
+                            average = 0;
+                            counter = 0;
+                            started = false;
+                            whatYouType.Text = "";
+                            toType.Text = "";
+                            whatYouType.Visible = false;
+                            toType.Visible = false;
+                            toTypeLabel.Visible = false;
+                            whatYouTypeLabel.Visible = false;
+                            total.Text = average.ToString() + "%";
+                            status.Text = "Not Playing";
+                            wrong = 0;
+                            wrongCounter = 0;
+                            return;
+                        }
+                        
+                        average = (float)Math.Round(average, 2);
+                        total.Text = average.ToString() + "%";
+                        letters[e.KeyChar] = e.KeyChar + " - " + average.ToString();
+                            
+                        for (int v = 0; v < listBox1.Items.Count; v++)
+                        {
+                            listBox1.Items.RemoveAt(v);
+                            v--;
+                        }
+
+                        foreach (var item in letters.Values)
+                        {
+                            listBox1.Items.Add(item);
+                        }
+
+                        if (wrongCounter == 1)
+                        {
+                            whatYouType.Text += e.KeyChar;
+                        }
+                        else if (wrongCounter >= 1)
+                        {
+                            whatYouType.Text = whatYouType.Text.Remove(whatYouType.Text.Length - 1,1);
+                            whatYouType.Text += e.KeyChar;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Capture key presses and send it to the Check_Key function to do futher evaluations
+        private void mainWindow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Check_Key(e);
+        }
+
+        // Capture key presses and send it to the Check_Key function to do futher evaluations. ListBox also captures keypresses because the main window isn't focused on at start.
+        private void accuracy_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Check_Key(e);
+        }
+    }
+}
